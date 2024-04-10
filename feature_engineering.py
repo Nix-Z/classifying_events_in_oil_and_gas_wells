3b888f9789d57e5dffcffa9363ada37ee6665421
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2024-04-09 21:52:01 -0700

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -14,6 +14,8 @@ def engineer_features():
     y.reset_index(drop=True, inplace=True)  # Reset index of the target variable
     scaled_data['class'] = y
 
+    data.to_csv('gas_well_events_cleansed_data.csv', index=False)
+
     return scaled_data
 
 # engineer_features()
